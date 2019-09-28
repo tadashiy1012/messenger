b8dc91bba1526145c88c0605ad5429136060eba2
TadashiYamazaki <yama3ki.tada4@gmail.com>
2019-09-28 21:26:17 +0900

    マルチタイムライン化

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -59,6 +59,40 @@ export default class TimeLine extends React.Component<TimeLineProps> {
                 </div>
             </li>
         });
-        return <ul css={{listStyleType:'none', padding:'0px'}}>{child}</ul>
+        const followId: Array<String> = ['259bbce0-e1e3-11e9-a32c-a132a86c2c90'];
+        const child2 = store!.timeLine.reverse().filter(e => {
+            const found = followId.find(ee => ee === e.authorId);
+            return found ? true:false;
+        }).map(e => {
+            const dt = new Date(e.date);
+            const name = store!.findAuthorname(e.authorId);
+            return <li key={e.id} css={{borderBottom:'solid 1px #ddd', padding:'6px'}}>
+                <div css={{display:'flex', alignItems:'center'}}>
+                    <img src={store!.findAuthorIcon(e.authorId)} width="24" height="24" css={{
+                        borderRadius:'20px', border:'solid 1px gray', margin: '4px'}}  />
+                    <span css={{margin:'0px 4px'}}>{name !== 'no_name' ? name : e.author}</span>
+                    <span css={{color:'#999', fontSize:'13px', margin:'0px 4px'}}>
+                        {dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate() + ' ' + dt.getHours() + ':' + dt.getMinutes()}
+                    </span>
+                </div>
+                <div css={{marginLeft:'22px', padding:'6px'}}>
+                    <span dangerouslySetInnerHTML={{__html: escape_html(e.say).replace('\n', '<br/>')}}></span>
+                </div>
+                <div css={{display:'flex', justifyContent:'space-around', fontSize:'11px', color:'#999'}}>
+                    <span>reply:</span>
+                    <span>favorite:</span>
+                </div>
+            </li>
+        });
+        return <div css={{display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
+            <div css={{width:store!.logged ? '46%':'88%'}}>
+                <h4>global timeline</h4>
+                <ul css={{listStyleType:'none', padding:'0px'}}>{child}</ul>
+            </div>
+            <div css={{width:'46%', display:store!.logged ? 'block':'none'}}>
+                <h4>local timeline</h4>
+                <ul css={{listStyleType:'none', padding:'0px'}}>{child2}</ul>
+            </div>
+        </div>
     }
 }
