f5a65376f03ce7518b300f652ac2ab15eb077fad
TadashiYamazaki <yama3ki.tada4@gmail.com>
2019-10-07 09:34:42 +0900

    cachesとusersのテストを追加

---
+++ tests/caches.test.ts
@@ -0,0 +1,11 @@
+import caches from '#/stores/caches';
+
+describe('caches', () => {
+    test('cachesが正常に機能する', () => {
+        expect(caches).not.toBeUndefined;
+        expect(caches.load()).resolves.toBe(true);
+        const cache = caches.getCaches;
+        expect(cache).not.toBeNull();
+        expect(cache).toHaveLength(0);
+    });
+});
---
+++ tests/users.test.ts
@@ -0,0 +1,11 @@
+import users from '#/stores/users';
+
+describe('users', () => {
+    test('usersが正常に機能する', () => {
+        expect(users).not.toBeUndefined;
+        expect(users.load()).resolves.toBe(true);
+        const list = users.getUsers;
+        expect(list).not.toBeNull;
+        expect(list).toHaveLength(0);
+    });
+});
