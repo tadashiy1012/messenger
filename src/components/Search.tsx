81501043bd7f3a60fdced4424d120ad18fa30515
TadashiYamazaki <yama3ki.tada4@gmail.com>
2019-10-14 12:00:52 +0900

    メッセージ一行を共通のコンポーネント化3

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,23 +3,61 @@ import * as React from 'react';
 import { css, jsx } from '@emotion/core';
 import { observer, inject } from 'mobx-react';
 import { history } from '../stores';
-import { SettingStoreType } from '../types';
-import { Finder, compareJson, getFullDateStr, escapeHtml } from '../utils';
+import { SettingStoreType, UserStoreType, SayType, UserType } from '../types';
+import { Finder, compareJson } from '../utils';
 import { Link } from 'react-router-dom';
+import Line from './Line';
+
+const liStyle = css({borderBottom:'solid 1px #ccc', padding:'16px', cursor:'pointer', '&:hover':{backgroundColor: '#eee'}});
+const bodyStyle = css({display:'grid', gridTemplateColumns:'180px 320px 230px'});
+const imgStyle = css({borderRadius:'36px', border:'solid 1px gray'});
+const flexCenter = css({display:'flex', alignItems:'center'});
+const margin4px = css({margin:'4px'});
+
+const UserLi = ({usr, count}: {usr: UserType, count: number}) => (
+    <li css={liStyle}>
+        <div css={bodyStyle} onClick={() => {
+            history.push({pathname:'/user', search:'?tgt=' + usr.serial})
+        }}>
+            <div css={flexCenter}>
+                <Link to={{pathname:'/user', search: '?tgt=' + usr.serial}} css={flexCenter} onClick={(ev) => {
+                    ev.stopPropagation();
+                }}>
+                    <img src={usr.icon} alt="icon" width="36" height="36" css={imgStyle} />
+                    <span css={margin4px}>{usr.name}</span>
+                </Link>
+            </div>
+            <div css={flexCenter}><span css={{margin:'6px'}}>{usr.profile || ''}</span></div>
+            <div css={flexCenter}> 
+                <span css={margin4px}>say:{count}</span>
+                <span css={margin4px}>follow:{usr.follow.length}</span>
+                <span css={margin4px}>follower:{usr.follower.length}</span>
+            </div>
+        </div>
+    </li>
+);
 
 interface SearchProps {
     setting?: SettingStoreType
+    user?: UserStoreType
 }
 
-interface SearchState {}
+interface SearchState {
+    flg: Boolean
+    sayCount: [string, number][]
+}
 
-@inject('setting')
+@inject('setting', 'user')
 @observer
 export default class Search extends React.Component<SearchProps, SearchState> {
     private unlisten: any;
     private prevQuery: any;
     constructor(props: Readonly<SearchProps>) {
         super(props);
+        this.state = {
+            flg: true,
+            sayCount: []
+        };
         this.unlisten = history.listen((location) => {
             const {setting} = this.props;
             if (location.pathname === '/search' && location.search) {
@@ -28,74 +66,62 @@ export default class Search extends React.Component<SearchProps, SearchState> {
                     this.prevQuery = [...query];
                     const tgtWord = query[0].split('=')[1];
                     setting!.setShowSearchTarget(tgtWord);
+                    this.setState({flg: true});
                 }
             }
         });
     }
+    likeClickHandler(tgt: SayType) {
+        const {user} = this.props;
+        user!.updateUserLike(tgt).catch(err => console.error(err));
+    }
+    unLikeClickHandler(tgt: SayType) {
+        const {user} = this.props;
+        user!.updateUserUnLike(tgt).catch(err => console.error(err));
+    }
     render() {
-        const {setting} = this.props;
+        const {setting, user} = this.props;
         let child = null;
-        let result = [];
+        let count = 0;
         if (setting!.showSearchMode === 0) {
-            result = Finder.searchSay(setting!.showSearchTarget || '');
-            child = result.reverse().map((e) => {
+            const crntUser = user!.currentUser;
+            const src = Finder.searchSay(setting!.showSearchTarget || '');
+            child = src.reverse().map((e) => {
+                const alike = crntUser && e.like.find(ee => ee === crntUser!.serial) ? 
+                    <i className="material-icons" css={{cursor:'pointer'}} onClick={() => {
+                        this.unLikeClickHandler(e)}}>favorite</i> :
+                    <i className="material-icons" css={{cursor:'pointer'}} onClick={() => {
+                        this.likeClickHandler(e)}}>favorite_border</i>;
+                const naLike = crntUser && e.like.find(ee => ee === crntUser!.serial) ? 
+                    <i className="material-icons">favorite</i> :
+                    <i className="material-icons">favorite_border</i>;
+                const like = user!.logged && crntUser && crntUser.serial !== e.authorId ? alike : naLike;
                 const name = Finder.findAuthorName(e.authorId);
-                return <li key={e.id} css={{borderBottom:'solid 1px #ddd', padding:'6px', cursor:'pointer', '&:hover':{backgroundColor: '#eee'}}}>
-                    <div onClick={() => { history.push({pathname:'/message', search:'?tgt=' + e.id}); }}>
-                        <div css={{display:'flex', alignItems:'center'}}>
-                            <Link to={{pathname:'/user', search: '?tgt=' + e.authorId}} css={{display:'flex', alignItems:'center'}} onClick={(ev) => {
-                                ev.stopPropagation();
-                            }}>
-                                <img src={Finder.findAuthorIcon(e.authorId)} width="24" height="24" css={{
-                                    borderRadius:'20px', border:'solid 1px gray', margin: '4px'}}  />
-                                <span css={{margin:'0px 4px'}}>{name !== 'no_name' ? name : e.author}</span>
-                            </Link>
-                            <span css={{color:'#999', fontSize:'13px', margin:'0px 4px'}}>
-                                {getFullDateStr(e.date)}
-                            </span>
-                        </div>
-                        <div css={{marginLeft:'22px', padding:'6px'}}>
-                            <span dangerouslySetInnerHTML={{__html: escapeHtml(e.say).replace('\n', '<br/>')}}></span>
-                        </div>
-                        <div css={{display:'flex', justifyContent:'space-around', fontSize:'11px', color:'#999'}}>
-                            <div css={{display:'flex', alignItems:'center'}}>
-                                <i className="material-icons">message</i>
-                                <span>reply:{e.reply.length}</span>
-                            </div>
-                            <div css={{display:'flex', alignItems:'center'}}>
-                                <i className="material-icons">favorite</i>
-                                <span>like:{e.like.length}</span>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                const icon = Finder.findAuthorIcon(e.authorId);
+                return <Line key={e.id} name={name} authorIcon={icon} say={e} likeIcon={like} />
             });
+            count = child.length;
         } else {
-            result = Finder.searchUser(setting!.showSearchTarget || '');
-            child = result.reverse().map((e) => {
-                return <li key={e.serial} css={{borderBottom:'solid 1px #ccc', padding:'16px', cursor:'pointer', '&:hover':{backgroundColor: '#eee'}}}>
-                    <div css={{display:'grid', gridTemplateColumns:'180px 320px 230px'}} onClick={() => {
-                        history.push({pathname:'/user', search:'?tgt=' + e.serial})
-                    }}>
-                        <div css={{display:'flex', alignItems:'center'}}>
-                            <Link to={{pathname:'/user', search: '?tgt=' + e.serial}} css={{
-                                display:'flex', alignItems:'center'
-                            }} onClick={(ev) => {
-                                ev.stopPropagation();
-                            }}>
-                                <img src={e.icon} alt="icon" width="36" height="36" css={{borderRadius:'36px', border:'solid 1px gray'}} />
-                                <span css={{margin:'4px'}}>{e.name}</span>
-                            </Link>
-                        </div>
-                        <div css={{display:'flex', alignItems:'center'}}><span css={{margin:'6px'}}>{e.profile || ''}</span></div>
-                        <div css={{display:'flex', alignItems:'center'}}> 
-                            <span css={{margin:'4px'}}>say:{0}</span>
-                            <span css={{margin:'4px'}}>follow:{e.follow.length}</span>
-                            <span css={{margin:'4px'}}>follower:{e.follower.length}</span>
-                        </div>
-                    </div>
-                </li>
+            const src = Finder.searchUser(setting!.showSearchTarget || '');
+            const tasks = src.map(e => {
+                return new Promise<[string, number]>((resolve) => {
+                    (async () => {
+                        resolve([e.serial, (await Finder.findUserSay(e.serial)).length]);
+                    })();
+                });
             });
+            Promise.all(tasks).then((results) => {
+                if (this.state.flg) {
+                    this.setState({sayCount: results, flg: false});
+                }
+            });
+            child = src.reverse().map((e) => {
+                let count = 0;
+                const tgt = this.state.sayCount.find(ee => ee[0] === e.serial);
+                if (tgt) count = tgt[1];
+                return <UserLi key={e.serial} usr={e} count={count} />
+            });
+            count = child.length;
         }
         return <React.Fragment>
             <div>
@@ -108,21 +134,11 @@ export default class Search extends React.Component<SearchProps, SearchState> {
                 }}>user search</button>
             </div>
             <p>keyword "<b>{setting!.showSearchTarget}</b>" mode: 
-                {setting!.showSearchMode === 0 ? 'message':'user'} result count: {result.length}</p>
+                {setting!.showSearchMode === 0 ? 'message':'user'} result count: {count}</p>
             <ul css={{listStyleType:'none', paddingLeft:'0px'}}>
                 {child}
             </ul>
         </React.Fragment>
-    }
-    componentDidMount() {
-        const {setting} = this.props;
-        if (history && setting) {
-            const params = history.location.search.substring(1).split('&');
-            if (params.length > 0) {
-                const tgtWord = params[0].split('=')[1];
-                setting.setShowSearchTarget(tgtWord);
-            }
-        }
     }
     componentWillUnmount() {
         this.unlisten();
